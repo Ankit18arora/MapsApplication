4c3f7c9845869c6a7d2255fbf8d90d99f479a3f7
Ankit18arora <ankitaroracse@gmail.com>
2016-08-04 14:50:39 +0530

    Initial commit

---
+++ app/src/main/java/com/sybernetics/mapsapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.sybernetics.mapsapplication;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
